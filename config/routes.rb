5fc0a8190ab90e2445942aaa21da14a1efd91718
Alex Wertheim <alex.wertheim@hey.com>
2021-12-17 07:09:44 +0100

    Enabled used sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get "features" => "features#index"
 
   resources :posts
+
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "signup" => "users#new"
+
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_063730) do
+ActiveRecord::Schema.define(version: 2021_12_16_081144) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_12_14_063730) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
