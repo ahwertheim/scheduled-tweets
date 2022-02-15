8df19fc72f0aae1615533a8b802e8f0b1e0e7dfd
Alex Wertheim <alex.wertheim@hey.com>
2022-02-15 07:44:26 +0100

    I think I have fixed Sidekiq. About to push to Heroku

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -1,7 +1,13 @@
 class TweetJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(tweet_id)
+    tweet = Tweet.find_by(id: tweet_id)
+    return if tweet.published?
+
+    # Reschedule a tweet further in the future -> will ignore the initially scheduled tweet
+    return if tweet.publish_at > Time.current
+
+    tweet.publish_to_twitter!
   end
 end
